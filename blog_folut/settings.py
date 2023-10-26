d2d6c4becc0613cfd6a87fd55beb81f38023a58a
Efosa <efosacharlesabu@gmail.com>
2023-10-26 22:28:19 +0100

    fix: AWS configuration -- Joshua1

--- blog_folut/settings.py
+++ blog_folut/settings.py
@@ -148,7 +148,7 @@ if USE_S3:
     # aws settings
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME", "blog-folut-bucket")
     AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     AWS_S3_OBJECT_PARAMETERS = {
